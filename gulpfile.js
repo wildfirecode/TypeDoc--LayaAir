2ba6b4b6ab1146e35770c5f18cb6f8b412d93b10
王鸿源honingwon <honingwon@gmail.com>
2017-08-22 17:41:35 +0800

    bug fix

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,34 @@ const stripJsonComments = require("strip-json-comments");
 
 //load tsconfig
 const tsconfigStr = fs.readFileSync('tsconfig.json').toString();
-const tsconfig = JSON.parse(stripJsonComments(tsconfigStr));
-let compilerOptions = tsconfig.compilerOptions;
 
-gulp.task('default', () => {
+const getGlobs = () => {
     const globs = [];
     globs.push('./src');
     globs.push('./libs/*.d.ts');//add third-part declarations
+    return globs;
+};
 
-    Object.assign(compilerOptions, {
-        name: 'LayaAir-TypeDoc-Demo',
-        includeDeclarations: false, //exclude LayaAir declaration file
-        version: true,
-        mode: 'file',
-        out: './typedocs/LayaAir-TypeDoc-Demo',
-        theme: 'minimal'
-    });
+const params = {
+    name: 'LayaAir-TypeDoc-Demo',
+    includeDeclarations: false, //exclude LayaAir declaration file
+    version: true,
+    mode: 'file',
+    out: './typedocs/LayaAir-TypeDoc-Demo',
+    theme: 'minimal'
+};
 
-    const stream = gulp.src(globs)
-        .pipe(typedoc(compilerOptions));
-    return stream;
+const getCompilerOptions = () => {
+    const tsconfig = JSON.parse(stripJsonComments(tsconfigStr));
+    const compilerOptions = tsconfig.compilerOptions;
+    return Object.assign(compilerOptions, params);
+}
+
+const generateDoc = () =>
+    gulp.src(getGlobs())
+        .pipe(typedoc(getCompilerOptions()));
+
+gulp.task('default', () => {
+    generateDoc();
+    gulp.watch('./src/*.ts', generateDoc);
 })
--- src/Bar.ts
+++ src/Bar.ts
@@ -4,7 +4,7 @@ namespace baz {
      * @author someone
      * @version 1.0.0
      */
-    export class Bar {
+    export class Bar2 {
         /**
          * Comment for method foo.
          * @param target Comment for param target.
